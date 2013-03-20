b89b18c94db87028fe9d4330efe394232d777f2c
James Touton <bekenn@gmail.com>
2013-03-20 09:14:24 +0000

    Test app displays a window.
    git-svn-id: http://zigserv/svn/Projects/trunk/wcdx@253 41632dc1-7a16-0410-8441-a1f66f767317

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,7 +1,50 @@
 #include "common.h"
 #include <wcdx.h>
 
+
+static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCommandLine, int nCmdShow)
 {
-	return EXIT_SUCCESS;
+	WNDCLASSEX wc =
+	{
+		sizeof(WNDCLASSEX),
+		0,
+		WindowProc,
+		0, 0,
+		hInstance,
+		nullptr,
+		::LoadCursor(nullptr, IDC_ARROW),
+		reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1),
+		nullptr,
+		L"Wcdx Test Window",
+		nullptr
+	};
+
+	ATOM wcAtom = ::RegisterClassEx(&wc);
+
+	HWND window = ::CreateWindowEx(0, MAKEINTATOM(wcAtom), L"Wcdx Test", WS_OVERLAPPEDWINDOW & ~(WS_THICKFRAME | WS_MAXIMIZEBOX), CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768, nullptr, nullptr, hInstance, nullptr);
+	::ShowWindow(window, nCmdShow);
+
+	MSG message;
+	BOOL result;
+	while ((result = ::GetMessage(&message, nullptr, 0, 0)) != 0)
+	{
+		if (result == -1)
+			return EXIT_FAILURE;
+		::DispatchMessage(&message);
+	}
+
+	return message.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		::PostQuitMessage(EXIT_SUCCESS);
+	}
+
+	return ::DefWindowProc(hWnd, uMsg, wParam, lParam);
 }

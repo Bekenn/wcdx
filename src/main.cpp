31dca1e83ddc0a52b1ff4cbb2cfbbbd29c7ef8d6
James Touton <bekenn@gmail.com>
2013-05-17 08:49:54 +0000

    Added missing file.
    git-svn-id: http://zigserv/svn/Projects/trunk/wcdx@305 41632dc1-7a16-0410-8441-a1f66f767317

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "common.h"
+
+HINSTANCE DllInstance;
+
+BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DllInstance = hInstDLL;
+		break;
+	}
+
+	return TRUE;
+}

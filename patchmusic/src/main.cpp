c8597992a5be3cfd359e5684af7b63dcc3ab5a01
James Touton <bekenn@gmail.com>
2014-12-24 10:07:33 +0000

    Delooped the tragedy sound (not sure if this is correct, but seems better than looping).
    git-svn-id: http://zigserv/svn/Projects/trunk/wcdx@398 41632dc1-7a16-0410-8441-a1f66f767317

--- patchmusic/src/main.cpp
+++ patchmusic/src/main.cpp
@@ -16,13 +16,34 @@ struct chunk_header
 	uint32_t chunk_link_index;
 };
 
-static void show_usage(const wchar_t* name);
-
-static constexpr chunk_header target_chunk =
+template <typename T, size_t length>
+constexpr size_t lengthof(const T (&a)[length])
 {
-	0x01AEAA68, 0x01B04410,
-	0, 2565,
-	1, 3433
+	return length;
+}
+
+static void show_usage(const wchar_t* name);
+bool deloop_chunk(filebuf& file, uint32_t offset, const chunk_header& target_chunk);
+
+
+static constexpr uint32_t target_chunk_offsets[] =
+{
+	0x03A1C7BC,
+	0x03A1C894
+};
+
+static constexpr chunk_header target_chunks[] =
+{
+	{
+		0x01A28DA0, 0x01A40058,
+		0, 2565,
+		1, 3419
+	},
+	{
+		0x01AEAA68, 0x01B04410,
+		0, 2565,
+		1, 3433
+	}
 };
 
 int wmain(int argc, wchar_t* argv[])
@@ -47,24 +68,35 @@ int wmain(int argc, wchar_t* argv[])
 		return EXIT_FAILURE;
 	}
 
-	auto position = file.pubseekoff(0x03a1c894, ios_base::beg);
+	for (size_t n = 0; n < lengthof(target_chunks); ++n)
+	{
+		if (!deloop_chunk(file, target_chunk_offsets[n], target_chunks[n]))
+			return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+bool deloop_chunk(filebuf& file, uint32_t offset, const chunk_header& target_chunk)
+{
+	auto position = file.pubseekoff(offset, ios_base::beg);
 	if (position == streampos(streamoff(-1)))
 	{
 		wcerr << L"Error: Seek error." << endl;
-		return EXIT_FAILURE;
+		return false;
 	}
 
 	chunk_header header;
 	if (file.sgetn(reinterpret_cast<char*>(&header), sizeof(header)) != sizeof(header))
 	{
 		wcerr << L"Error: Unexpected end of file." << endl;
-		return EXIT_FAILURE;
+		return false;
 	}
 
 	if (memcmp(&header, &target_chunk, sizeof(chunk_header)) != 0)
 	{
 		wcerr << L"Error: Bytes in stream chunk header do not match expected values." << endl;
-		return EXIT_FAILURE;
+		return false;
 	}
 
 	header.chunk_link_count = 0;
@@ -75,10 +107,10 @@ int wmain(int argc, wchar_t* argv[])
 	if (file.sputn(reinterpret_cast<char*>(&header), sizeof(header)) != sizeof(header))
 	{
 		wcerr << L"Error: Couldn't write to file." << endl;
-		return EXIT_FAILURE;
+		return false;
 	}
 
-	return EXIT_SUCCESS;
+	return true;
 }
 
 void show_usage(const wchar_t* name)
